c476b1dca7c107ec8356ca903c74106ced961185
Michal Ptaszek <michal.ptaszek@erlang-consulting.com>
2009-12-23 12:43:05 +0100

    Simple Hello World from Python works

--- c_src/hello_python.c
+++ c_src/hello_python.c
@@ -1,3 +1,18 @@
+#import "Python.h"
+
 char *hello_from_python() {
-  return "DUPADUPADUPA";
+  PyRun_SimpleString("from pytherl import pytherl");
+  PyRun_SimpleString("result_string = pytherl.hello()");
+
+  PyObject *module = PyImport_AddModule("__main__");
+  assert(module);
+  
+  PyObject *dictionary = PyModule_GetDict(module);
+  assert(dictionary);
+
+  PyObject *result = PyDict_GetItemString(dictionary, "result_string");
+  assert(result);
+  assert(PyString_Check(result));
+
+  return PyString_AsString(result);
 };
