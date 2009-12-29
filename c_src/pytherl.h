48b729a56b19643ff047a12ea4eda6f08d276872
Michal Ptaszek <michal.ptaszek@erlang-consulting.com>
2009-12-29 14:36:12 +0100

    Handling Python list is now working

--- c_src/pytherl.h
+++ c_src/pytherl.h
@@ -9,19 +9,11 @@
 // pytherl_utils
 void erl_list_to_string(ErlNifEnv *env, ERL_NIF_TERM list, char *string);
 char *erl_arg_list_to_string(ErlNifEnv *env, ERL_NIF_TERM list, int *arg_size);
-void erl_list_to_py_list(ErlNifEnv *env, ERL_NIF_TERM list, PyObject *pyList);
-PyObject *get_none();
 
 // pytherl_interpreter
 PyObject *pytherl_call(char *mod, char *fun, char *args, int arg_size);
 
 // pytherl_converter
 ERL_NIF_TERM py_to_erl(ErlNifEnv* env, PyObject *pyObj);
-
-// hello_python
-char *hello_from_python();
-
-// re_run_python
-int re_match_py(char *, char *);
 
 #endif // _PYTHERL_H_
--- c_src/pytherl_converter.c
+++ c_src/pytherl_converter.c
@@ -1,6 +1,21 @@
 #import "pytherl.h"
 
-// TODO - implement tuple and list convertions
+// TODO - implement tuple convertions
+
+ERL_NIF_TERM pytherl_make_list(ErlNifEnv *env, PyObject *obj) {
+  Py_ssize_t size = PyList_Size(obj), i;
+  ERL_NIF_TERM list = enif_make_list(env, 0);
+  
+  i = size-1;
+  while(i >= 0) {
+    ERL_NIF_TERM head = py_to_erl(env, PyList_GetItem(obj, i));
+    list = enif_make_list_cell(env, head, list);
+
+    i--;
+  };
+
+  return list;
+}
 
 ERL_NIF_TERM py_to_erl(ErlNifEnv *env, PyObject *pyObj) {
   if(PyInt_Check(pyObj)) {
@@ -9,6 +24,8 @@ ERL_NIF_TERM py_to_erl(ErlNifEnv *env, PyObject *pyObj) {
     return enif_make_atom(env, Py_True == pyObj ? "true" : "false");
   } else if(PyString_Check(pyObj)) {
     return enif_make_string(env, PyString_AsString(pyObj));
+  } else if(PyList_Check(pyObj)) {
+    return pytherl_make_list(env, pyObj);
   };
 
   return enif_make_badarg(env);
